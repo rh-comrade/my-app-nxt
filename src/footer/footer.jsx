6919dcd373b770285172b94e81bdd363537a7e1c
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-05-07 18:44:58 +0530

    initial commit

---
+++ src/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./footer.module.css"
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <p>&copy;2024</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./header.module.css"
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>welcome to shopper-x</h1>
+    </div>
+  )
+}
+
+export default Header;
