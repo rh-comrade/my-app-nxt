9a984a42647be98eae81ffadc961cbb4f65624c5
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-05-08 12:17:38 +0530

    us001: home about and contact pages added

---
+++ src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './about.module.css'
+import { Menu } from '@/menu'
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <Menu/>
+      about page
+    </div>
+  )
+}
+
+export default About
